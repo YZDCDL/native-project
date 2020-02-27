b394e815d34eabdac4abb7024bbbb3e642576a71
Yzd <13996615914@139.com>
2020-02-27 14:59:23 +0800

    frist commit

---
+++ native_project/native_project/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  native_project
+//
+//  Created by YinZhongDong on 2020/2/27.
+//  Copyright © 2020 YinZhongDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ native_project/native_project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  native_project
+//
+//  Created by YinZhongDong on 2020/2/27.
+//  Copyright © 2020 YinZhongDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
